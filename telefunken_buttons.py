e4716a58088c51b86b0ee62d19e471b3da44ff6d
Adrien Plaçais <28395365+AdrienPlacais@users.noreply.github.com>
2025-01-19 12:58:38 +0100

    add object to store the telefunken buttons

---
+++ telefunken_buttons.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Define an object to handle the set of five buttons in the front."""
+
+import sys
+import time
+from collections.abc import Sequence
+
+import RPi.GPIO as GPIO
+from button_class import Button, interrupt
+
+
+class TeleButtons:
+    """Implement five buttons.
+
+    They are mutually exclusive: pressing one disables the previously activated
+    one. Two buttons have a specific behavior:
+        - The first does not stay in the down position. Used to shutdown the
+        system.
+        - The last does not disable the other button. Used to activate a 220V
+        disco light thanks to a relay.
+
+    """
+
+    def __init__(self, buttons: Sequence[Button]) -> None:
+        """Create object from several buttons."""
+        self.buttons = buttons
+
+    @property
+    def off(self) -> Button:
+        """Give the leftmost button."""
+        return self.buttons[0]
+
+    @property
+    def fip(self) -> Button:
+        """Give the 2nd button, starting from left."""
+        return self.buttons[1]
+
+    @property
+    def spotify(self) -> Button:
+        """Give the 3rd button, starting from left."""
+        return self.buttons[2]
+
+    @property
+    def unused(self) -> Button:
+        """Give the 4th button, starting from left."""
+        return self.buttons[3]
+
+    @property
+    def disco(self) -> Button:
+        """Give the last button."""
+        return self.buttons[4]
+
+
+if __name__ == "__main__":
+    from config_class import Configuration
+    from log_class import Log
+
+    config = Configuration()
+    log = Log()
+    if len(log.getName()) < 1:
+        log.init("radio")
+
+    pullupdown = ["DOWN", "UP"]
+
+    print("Test TelefunkenButtons class")
+
+    # Get switch configuration
+    off_switch = config.getSwitchGpio("off")
+    fip_switch = config.getSwitchGpio("fip")
+    spotify_switch = config.getSwitchGpio("spotify")
+    unused_switch = config.getSwitchGpio("unused")
+    disco_switch = config.getSwitchGpio("disco")
+
+    print(f"off_switch GPIO: {off_switch}")
+    print(f"fip_switch GPIO: {fip_switch}")
+    print(f"spotify_switch GPIO: {spotify_switch}")
+    print(f"unused_switch GPIO: {unused_switch}")
+    print(f"disco_switch GPIO: {disco_switch}")
+
+    Button(off_switch, interrupt, log, config.pull_up_down)
+    Button(fip_switch, interrupt, log, config.pull_up_down)
+    Button(spotify_switch, interrupt, log, config.pull_up_down)
+    Button(unused_switch, interrupt, log, config.pull_up_down)
+    Button(disco_switch, interrupt, log, config.pull_up_down)
+
+    try:
+        while True:
+            time.sleep(0.2)
+
+    except KeyboardInterrupt:
+        print(" Stopped")
+        GPIO.cleanup()
+        sys.exit(0)
